8a0ccea1671fb75e915039226e271631b744d629
Ian Maddison <ian@i.need.stronger.coffee>
2019-07-11 00:43:19 +0100

    Move tests from dhtengine to rdfendpoint

--- src/main/java/com/goodforgoodbusiness/engine/crypto/EncryptionException.java
+++
@@ -1,9 +0,0 @@
-package com.goodforgoodbusiness.engine.crypto;
-
-public class EncryptionException extends Exception {
-	private static final long serialVersionUID = 1L;
-
-	public EncryptionException(String message, Throwable cause) {
-		super(message, cause);
-	}
-}
